9196a13cf7c7510e4bb87b4d2b0e422accb871bf
aarafkhan360-netizen <aaraf.khan360@gmail.com>
2026-07-31 01:49:00 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDlHWxIQ6lxWJ6wSB5UxHqJpa8WEtmdeEE",
+  authDomain: "viralhub-bd-3a679.firebaseapp.com",
+  projectId: "viralhub-bd-3a679",
+  storageBucket: "viralhub-bd-3a679.firebasestorage.app",
+  messagingSenderId: "603360533030",
+  appId: "1:603360533030:web:dfa5aa9fa6cf306cc9e4e7",
+  measurementId: "G-1GH867GEX7"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
 document.addEventListener("DOMContentLoaded", () => {
 
   const buttons = document.querySelectorAll("button");
@@ -8,7 +29,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   buttons[1].addEventListener("click", () => {
     const comment = prompt("আপনার মন্তব্য লিখুন:");
-    if(comment){
+    if (comment) {
       alert("ধন্যবাদ!\nআপনার মন্তব্য: " + comment);
     }
   });
@@ -25,9 +46,15 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-});
-const loginBtn = document.getElementById("loginBtn");
+  const loginBtn = document.getElementById("loginBtn");
 
-loginBtn.addEventListener("click", () => {
-  alert("Google Login বাটন কাজ করছে!");
+  loginBtn.addEventListener("click", async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      alert("স্বাগতম " + result.user.displayName);
+    } catch (error) {
+      alert(error.message);
+    }
+  });
+
 });
